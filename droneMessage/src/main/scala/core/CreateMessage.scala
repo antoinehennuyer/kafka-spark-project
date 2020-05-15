ac7def7a51ade0e9e82553fc487f3524848bdf13
initwren <kevin.bourges@epita.fr>
2020-05-15 13:13:49 +0200

    [DroneMessage] generate random message

--- droneMessage/src/main/scala/core/CreateMessage.scala
+++ droneMessage/src/main/scala/core/CreateMessage.scala
@@ -5,13 +5,49 @@ package core
 //import ExecutionContext.Implicits.global
 import utils.MessageUtils
 import utils.MessageUtils._
-//import scala.util.Random
-//import Random.nextInt
-//import java.time.LocalDateTime
-//import java.time.format.DateTimeFormatter
+import scala.util.Random
+import Random.nextInt
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 object CreateMessage {
+  var violation = 0
+  var alert = 0
+  var message = 0
 
+  def RandomMessage(nbr: Int, idDrone: String): Any = {
+    nbr match {
+      case 0 => {
+        println("violation: ", violation)
+        println("alert: ", alert)
+        println("message: ", message)
+      }
+      case _ => {
+        val date = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm").format(LocalDateTime.now())
+//        Thread.sleep(5000)
+        val randomType = nextInt(100)
+        if (randomType < 25) {
+          val typeAlert = nextInt(100)
+          if (typeAlert == 0) {
+            val newMessage = MessageGenerate(idDrone, "Paris", date, "alert")
+            println(newMessage)
+            alert += 1
+          }
+          else {
+            val newMessage = MessageGenerate(idDrone, "Paris", date, "violation")
+            println(newMessage)
+            violation += 1
+          }
+        }
+        else {
+          val newMessage = MessageGenerate(idDrone, "Paris", date, "regularly message")
+          println(newMessage)
+          message += 1
+        }
+        RandomMessage(nbr - 1, idDrone)
+      }
+    }
+  }
 
   def MessageGenerate(id: String, loc: String, time: String, vioCode: String): Message = {
     MessageUtils.Message(id, loc, time, vioCode)
