a23edea56908f479c0c1f8c2a36db73dbea68598
initwren <kevin.bourges@epita.fr>
2020-05-28 11:46:27 +0200

    [analysis] make 3 analysis on database

--- analysis/src/main/scala/analyser.scala
+++ analysis/src/main/scala/analyser.scala
@@ -11,9 +11,21 @@ object analyser{
     val conf = new SparkConf().setAppName("Analyser").setMaster("local[*]")
     val spark = SparkSession.builder().config(conf).getOrCreate()
     import spark.implicits._
-    val df = spark.read.csv("result.csv").toDF()
-    df.show()
+    val df = spark.read.option("header", "true").csv("Parking_Violations_Issued_-_Fiscal_Year_2017.csv").toDF()
+    // df.show()
     // RUN ANALYSIS HERE
+
+    // val violation = df.groupBy("violation_code").count()
+
+    val state = df.groupBy("Registration State").count().orderBy(desc("count"))
+    val carType = df.groupBy("Vehicle Make").count().orderBy(desc("count"))
+    val infraction = df.groupBy("Violation Code").count().orderBy(desc("count"))
+    state.show(5)
+    carType.show(5)
+    infraction.show(5)
+    // violation.where("count > 3").show()
+    // val test = df.filter($"violation_code" === "alert").groupBy("violation_code").count().show()
+    // val violation2 = df.groupBy("violation_code").where("violation_code == test").count().show()
     spark.close()
   }
 
