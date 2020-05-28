06d5e2a2f00b96ce6669f53585e55d2ee2dd288d
hugotritsch <hugo.tritsch@epita.fr>
2020-05-28 19:02:36 +0200

    [stream-to-hdfs] add function saveCsv

--- stream_to_hdfs/src/main/scala/stream_to_hdfs.scala
+++ stream_to_hdfs/src/main/scala/stream_to_hdfs.scala
@@ -1,82 +1,27 @@
 
-import org.apache.spark
-import org.apache.spark.SparkConf
-import org.apache.spark.{SparkConf, SparkContext, sql}
-import org.apache.spark.rdd.RDD
-import org.apache.spark.sql.types.{DataTypes, DoubleType, IntegerType, StringType, StructField, StructType}
-import org.apache.spark.sql.{DataFrame, SparkSession}
-import org.apache.spark.sql.functions._
+ import org.apache.spark
+ import org.apache.spark.SparkConf
+ import org.apache.spark.{SparkConf, SparkContext, sql}
+ import org.apache.spark.rdd.RDD
+ import org.apache.spark.sql.types.{DataTypes, DoubleType, IntegerType, StringType, StructField, StructType}
+ import org.apache.spark.sql.{DataFrame, SparkSession}
+ import org.apache.spark.sql.functions._
 
-object stream_to_hdfs {
+ import scala.concurrent.duration.Duration
 
-  def main(args: Array[String]) {
-    /* println( "Trying to write to HDFS..." )
-    val conf = new Configuration()
-    //conf.set("fs.defaultFS", "hdfs://quickstart.cloudera:8020")
-    conf.set("fs.hdfs.impl", classOf[org.apache.hadoop.hdfs.DistributedFileSystem].getName);
-    conf.set("fs.file.impl", classOf[org.apache.hadoop.fs.LocalFileSystem].getName);
-
-    val fs= FileSystem.get(conf)
-    val output = fs.create(new Path("/tmp/mySample.txt"))
-    val writer = new PrintWriter(output)
-    try {
-      writer.write("this is a test")
-      writer.write("\n")
-    }
-    finally {
-      writer.close()
-      println("Closed!")
-    }
-    println("Done!")
-  }*/
+ /*import akka.actor.Actor
+ import akka.actor.ActorSystem
+ import akka.actor.Props*/
 
 
+ object stream_to_hdfs {
 
-    val spark = SparkSession
-      .builder
-      .appName("Spark-Kafka-Integration")
-      .master("local")
-      .getOrCreate()
-
-//    val mySchema = StructType(Array(
-//      StructField("id", IntegerType),
-//      StructField("location", StringType),
-//    ))
-
+  def saveCsv(spark: SparkSession) = {
     import spark.implicits._
 
-    val df = spark
-      .read
-        .format("kafka")
-      .option("kafka.bootstrap.servers", "localhost:9092")
-      .option("subscribe", "general")
-      .load()
-
-    val JsonDf = df.selectExpr("CAST(value AS STRING)")
-    val struct = new StructType()
-      .add("ID", DataTypes.StringType)
-      .add("location", DataTypes.StringType)
-      .add("time",DataTypes.StringType)
-      .add("violation_code",DataTypes.StringType)
-      .add("state", DataTypes.StringType)
-      .add("vehiculeMake", DataTypes.StringType)
-      .add("batteryPercent", DataTypes.StringType)
-      .add("temperatureDrone", DataTypes.StringType)
-      .add("mType", DataTypes.StringType)
-
-    val valuedf = JsonDf.select(from_json($"value", struct).as("value"))
-    val valuesplit = valuedf.selectExpr("value.ID", "value.location","value.time","value.violation_code", "value.state", "value.vehiculeMake", "value.batteryPercent", "value.temperatureDrone", "value.mType")
-    valuesplit.write.format("csv").option("header", "true").save("test.csv")
-    //valuesplit.show(20)
-
-    //personJsonDf.show(20)
-
-    //df.show(10)
-
-    //CODE FOR CONSUMER CSV
     val dfCsv = spark
       .read
-        .format("kafka")
+      .format("kafka")
       .option("kafka.bootstrap.servers", "localhost:9092")
       .option("subscribe", "sendCSV")
       .load()
@@ -94,8 +39,79 @@ object stream_to_hdfs {
       .add("mType", DataTypes.StringType)
 
     val valuedfCsv = JsonDfCsv.select(from_json($"value", structCsv).as("value"))
-    val valuesplitCsv = valuedfCsv.selectExpr("value.ID", "value.location","value.time","value.violation_code", "value.state", "value.vehiculeMake", "value.batteryPercent", "value.temperatureDrone", "value.mType")
+    val valuesplitCsv = valuedfCsv.selectExpr("value.ID", "value.location","value.time","value.violation_code",
+      "value.state", "value.vehiculeMake", "value.batteryPercent", "value.temperatureDrone", "value.mType")
     valuesplitCsv.write.format("csv").option("header", "true").save("CSVFile.csv")
+  }
+
+  def main(args: Array[String]) {
+    val spark = SparkSession
+      .builder
+      .appName("Spark-Kafka-Integration")
+      .master("local")
+      .getOrCreate()
+
+    import spark.implicits._
+
+    val df = spark
+      .read
+      .format("kafka")
+      .option("kafka.bootstrap.servers", "localhost:9092")
+      .option("subscribe", "general")
+      .load()
+
+    val JsonDf = df.selectExpr("CAST(value AS STRING)")
+    val struct = new StructType()
+      .add("ID", DataTypes.StringType)
+      .add("location", DataTypes.StringType)
+      .add("time",DataTypes.StringType)
+      .add("violation_code",DataTypes.StringType)
+      .add("state", DataTypes.StringType)
+      .add("vehiculeMake", DataTypes.StringType)
+      .add("batteryPercent", DataTypes.StringType)
+      .add("temperatureDrone", DataTypes.StringType)
+      .add("mType", DataTypes.StringType)
+
+    val valuedf = JsonDf.select(from_json($"value", struct).as("value"))
+    val valuesplit = valuedf.selectExpr("value.ID", "value.location","value.time","value.violation_code",
+      "value.state", "value.vehiculeMake", "value.batteryPercent", "value.temperatureDrone", "value.mType")
+    valuesplit.write.format("csv").option("header", "true").save("test.csv")
+
+    //CODE FOR CONSUMER CSV
+    /*val actorSystem = ActorSystem()
+    val scheduler = actorSystem.scheduler
+    val task = new Runnable { def run() { saveCsv(spark) } }
+    implicit val executor = actorSystem.dispatcher*/
+    saveCsv(spark)
+
+    /*scheduler.schedule(
+      initialDelay = Duration(0, TimeUnit.SECONDS),
+      interval = Duration(10, TimeUnit.SECONDS),
+      runnable = task)*/
+    saveCsv(spark)
+    /*val dfCsv = spark
+      .read
+      .format("kafka")
+      .option("kafka.bootstrap.servers", "localhost:9092")
+      .option("subscribe", "sendCSV")
+      .load()
+
+    val JsonDfCsv = dfCsv.selectExpr("CAST(value AS STRING)")
+    val structCsv = new StructType()
+      .add("ID", DataTypes.StringType)
+      .add("location", DataTypes.StringType)
+      .add("time",DataTypes.StringType)
+      .add("violation_code",DataTypes.StringType)
+      .add("state", DataTypes.StringType)
+      .add("vehiculeMake", DataTypes.StringType)
+      .add("batteryPercent", DataTypes.StringType)
+      .add("temperatureDrone", DataTypes.StringType)
+      .add("mType", DataTypes.StringType)
+
+    val valuedfCsv = JsonDfCsv.select(from_json($"value", structCsv).as("value"))
+    val valuesplitCsv = valuedfCsv.selectExpr("value.ID", "value.location","value.time","value.violation_code",
+      "value.state", "value.vehiculeMake", "value.batteryPercent", "value.temperatureDrone", "value.mType")
+    valuesplitCsv.write.format("csv").option("header", "true").save("CSVFile.csv")*/
   }
 
 
